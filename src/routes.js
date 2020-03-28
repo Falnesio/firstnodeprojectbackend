5b9605159fe412139e1a5d582059df1b4c985007
Falnesio <falnesio.ghander@economia.ufjf.br>
2020-03-27 23:40:03 -0300

    Criar paginação no IncidentController.js Criar SessionController para logins
    Signed-off-by: Falnesio <falnesio.ghander@economia.ufjf.br>

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
 //const connection = require('./database/connection');
 
@@ -131,6 +132,8 @@ routes.post("/ongs", OngController.create); /*async (request, response) => {
     });
 });
 */
+
+routes.post('/sessions', SessionController.create);
 
 routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
