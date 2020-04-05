c8f899602755ed806cc35575fd3e5078d17b9ddd
Falnesio <falnesio.ghander@economia.ufjf.br>
2020-04-05 13:24:06 -0300

    prepare for heroku
    Signed-off-by: Falnesio <falnesio.ghander@economia.ufjf.br>

--- src/server.js
+++ src/server.js
@@ -1,7 +1,15 @@
 const app = require('./app');
 
-appURL = 3333;
+//appURL = 3333;
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
+
+/*
 
 app.listen(
-    appURL
-);
+    //appURL
+    process.env.PORT
+);*/
